00366706805ebcf35b7042f50d88fcf19947dcf4
bhavesh06-ram <bshah911917@gmail.com>
2026-06-11 18:39:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+
+// Define a route handler for the homepage
+app.get('/',(req,res)=>{
+    res.send("hello bacho");
+})
+
+
+// Start listening for requests
+app.listen(process.env.PORT,'0.0.0.0',() => {
+  console.log(`Express server running at http://localhost:${process.env.PORT}/`);
+});
